1a9b4933ef2c3dc7df643df88c9f57fcb75f03ff
张昊辰 <13662143060@163.com>
2016-03-17 09:47:17 +0800

    Initial Commit

---
+++ pengyouquan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pengyouquan
+//
+//  Created by 张昊辰 on 16/3/17.
+//  Copyright © 2016年 com.mingxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pengyouquan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pengyouquan
+//
+//  Created by 张昊辰 on 16/3/17.
+//  Copyright © 2016年 com.mingxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
